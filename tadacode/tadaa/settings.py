66c50d6cffccf76e4a16e47fb232f12f593ea82f
Ahmad Alobaid <ahmad88me@gmail.com>
2018-05-18 16:53:48 +0200

    setup mysql options and engine

--- tadacode/tadaa/settings.py
+++ tadacode/tadaa/settings.py
@@ -83,8 +83,10 @@ if os.environ['db'] == 'sqlite':
 elif os.environ['db'] == 'mysql':
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.mysql',
+            'OPTIONS': {
+                'read_default_file': os.path.join(BASE_DIR, 'mysql.cnf'),
+            },
         }
     }
 
