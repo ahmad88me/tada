9bbf92b8cbcf3a26941f59e3e6c5c11c60704c72
Ahmad Alobaid <ahmad88me@gmail.com>
2018-08-10 17:07:17 +0200

    add api to test webcommons

--- tadacode/tadaa/urlsapi.py
+++ tadacode/tadaa/urlsapi.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 import viewsapi as views
 
 urlpatterns = [
+    url(r'webcommons_get_col_type', views.webcommons_get_col_type),
     url(r'type_entity_col', views.type_entity_col),
     url(r'get_col_type', views.get_col_type),
-    url(r'webcommons_get_col_type', views.webcommons_get_col_type),
+
 ]
