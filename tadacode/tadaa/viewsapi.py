585e7a802ff91cfe9b725460ef280e564d76fa44
Ahmad Alobaid <ahmad88me@gmail.com>
2018-08-10 17:15:33 +0200

    add api to test webcommons

--- tadacode/tadaa/viewsapi.py
+++ tadacode/tadaa/viewsapi.py
@@ -66,7 +66,7 @@ def get_col_type(request):
 def webcommons_get_col_type(request):
     if 'file_name' in request.GET:
         ann_run = AnnRun.objects.get(name__contains=request.GET['file_name'])
-        eann = ann_run.objects.entityann_set.all()[0]
+        eann = ann_run.entityann_set.all()[0]
         request.GET['id'] = str(eann.id)
         return get_col_type(request)
     else:
